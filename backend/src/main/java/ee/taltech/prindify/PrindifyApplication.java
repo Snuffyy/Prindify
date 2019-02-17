511ae9b87375dc580a8b7a410c99e88793b59496
Maksym Maliarov <maxim.maliarov@gmail.com>
2019-02-17 04:18:33 +0200

    Initial

---
+++ backend/src/main/java/ee/taltech/prindify/PrindifyApplication.java
@@ -0,0 +1,13 @@
+package ee.taltech.prindify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrindifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrindifyApplication.class, args);
+	}
+
+}
