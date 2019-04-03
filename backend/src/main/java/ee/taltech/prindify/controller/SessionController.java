e02682daf9c718df3c374d98e55c14a85d3710a8
sporadicCode <patsok@ttu.ee>
2019-04-03 03:51:19 +0300

    back-end test for session controller

---
+++ backend/src/main/java/ee/taltech/prindify/controller/SessionController.java
@@ -0,0 +1,15 @@
+package ee.taltech.prindify.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class SessionController {
+
+    @GetMapping("session")
+    public String getSessionId(HttpSession session){
+        return session.getId();
+    }
+}
