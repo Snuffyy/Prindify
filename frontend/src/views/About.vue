956ea21a3d36f76add2970d163df8cc5c7549b26
sporadicCode <patsok@ttu.ee>
2019-04-03 06:32:37 +0300

    NB! COMMIT BELONGS TO KOBURD, restoring it after a force push

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,73 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+
+    <div id="img-container">
+      <img id="img-background" src="http://rgho.st/6LRHLnl9v/thumb.png" width="500">
+    </div>
+
+    <h1>Print it with Prindify</h1>
+    <p>A modern platform for personalized T-shirts and jerseys, we are the go-to-place for anyone looking to realize their creative ideas on quality fabrics. We value freedom of expression, whether it’s with your own designs or those made available by our community. T-shirts can easily be personalized with images and custom text. Moreover, Prindify is an ideal custom merchandise partner: Emerging local stars and a large number of large companies and brands are among our esteemed customers. Our top-quality prints, custom t-shirts and services will make sure that your business hits it big.</p>
+    <div id="container">
+      <div>
+        <div id="img-container2">
+          <img id="img-background2" src="https://softwaretestingtech.com/wp-content/uploads/2018/12/KU273L0WTW-1024x684.jpg.jpg" width="500">
+        </div>
+
+        <h2>Few  reasons why you should choose Prindify!</h2>
+        <ul>
+          <li><a href="#">1. We Guarantee Our Uptime, Support and Services</a></li>
+          <li><a href="#">2. We Offer a 45 Day Money Back Guarantee</a></li>
+          <li><a href="#">3. Our Support Team is Friendly, Fast, and Knowledgeable</a></li>
+          <li><a href="#">4. Our Customers Are Our Biggest Asset</a></li>
+          <li><a href="#">5. Passion: Done with love!</a></li>
+          <li><a href="#">6. Attention to Detail!</a></li>
+        </ul>
+      </div>
+
+    </div>
   </div>
 </template>
+<style>
+  .about {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+  }
+
+
+  h2 {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    border-bottom: 1px solid #ccc;
+  }
+
+  li:last-child {
+    border: none;
+  }
+
+  li a {
+    text-decoration: none;
+    color: #000;
+    display: block;
+
+    -webkit-transition: font-size 0.3s ease, background-color 0.3s ease;
+    -moz-transition: font-size 0.3s ease, background-color 0.3s ease;
+    -o-transition: font-size 0.3s ease, background-color 0.3s ease;
+    -ms-transition: font-size 0.3s ease, background-color 0.3s ease;
+    transition: font-size 0.3s ease, background-color 0.3s ease;
+  }
+
+  li a:hover {
+    font-size: 30px;
+  }
+
+</style>
